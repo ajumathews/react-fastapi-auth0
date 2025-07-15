2caa4fc627d8d3b74d9fa5707618d06bb1227a62
ajumathews <aju.mathewsamuel@gmail.com>
2025-07-15 12:19:52 -0700

    Add react frontend

---
+++ front-end/app/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Main() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
