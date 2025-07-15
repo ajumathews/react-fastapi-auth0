2eb4e8b1c0cfcc7044171b430d94cedcfd102342
ajumathews <aju.mathewsamuel@gmail.com>
2025-07-15 13:12:43 -0700

    Update login

--- front-end/app/components/Login.js
+++ front-end/app/components/Login.js
@@ -1,14 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <form className="login">
-      <label htmlFor="username">email</label>
-      <input type="email" id="username"></input>
-      <label htmlFor="password">password</label>
-      <input type="password" id="password"></input>
-      <button type="submit">Login</button>
-    </form>
+    <div class="container">
+      <form onSubmit={handleSubmit} className="login">
+        <div class="login-input">
+          <label htmlFor="username">Email</label>
+          <input onChange={e => setEmail(e.target.value)} type="email" id="username"></input>
+        </div>
+        <div class="login-input">
+          <label htmlFor="password">Password</label>
+          <input onChange={e => setPassword(e.target.value)} type="password" id="password"></input>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 }
 
